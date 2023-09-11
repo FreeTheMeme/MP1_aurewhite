7dcffc95a8bf385608e0e936b815cbb8feed6bdb
bigtin <82232786+FreeTheMeme@users.noreply.github.com>
2023-09-11 15:23:57 -0400

    mileage.java done

---
+++ src/mileage/mileage.java
@@ -0,0 +1,26 @@
+package mileage;
+
+import java.util.Scanner;
+
+public class mileage {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        //vars
+        float miles;
+        float gallons_of_gas;
+        float mileage;
+
+        //input
+        System.out.println("Mileage Calculator V0.1");
+        System.out.println("enter distance (in miles):");
+        miles = scan.nextFloat();
+        System.out.println("enter gallons of gas:");
+        gallons_of_gas = scan.nextFloat();
+
+        //calc
+        mileage = miles/gallons_of_gas;
+
+        //output
+        System.out.println("Your Mileage is: " + mileage + " per gallon");
+    }
+}
