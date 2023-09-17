382fe7932c3b5b57a51ef965c669d2a442f2569a
bigtin <82232786+FreeTheMeme@users.noreply.github.com>
2023-09-17 17:13:09 -0400

    jar is done

--- src/jar/jar.java
+++ src/jar/jar.java
@@ -14,5 +14,22 @@ public class jar {
         int number_of_dimes;
         int number_of_nickels;
         int number_of_pennies;
+        int total_in_jar;
+
+        //input
+        System.out.println("Enter in the number of Quarters in the Jar:");
+        number_of_quarters = scan.nextInt();
+        System.out.println("Enter in the number of Dimes in the Jar:");
+        number_of_dimes = scan.nextInt();
+        System.out.println("Enter in the number of Nickels in the Jar:");
+        number_of_nickels = scan.nextInt();
+        System.out.println("Enter in the number of Pennies in the Jar:");
+        number_of_pennies = scan.nextInt();
+
+        //calc
+        total_in_jar =(number_of_quarters * quarter) + (number_of_dimes * dimes) + (number_of_nickels * nickels) + (number_of_pennies * pennies);
+
+        //output
+        System.out.println("The jar contains "+ total_in_jar +" cents");
     }
 }
