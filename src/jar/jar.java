1896f29c2eed8a4f27d110c5a3fb3c576c6c6684
bigtin <82232786+FreeTheMeme@users.noreply.github.com>
2023-09-17 16:46:22 -0400

    jar is here

---
+++ src/jar/jar.java
@@ -0,0 +1,18 @@
+package jar;
+
+import java.util.Scanner;
+
+public class jar {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        //vars
+        int quarter = 25;
+        int dimes = 10;
+        int nickels = 5;
+        int pennies = 1;
+        int number_of_quarters;
+        int number_of_dimes;
+        int number_of_nickels;
+        int number_of_pennies;
+    }
+}
